7e25b4e185fdb542447cddd1bcca7e5e215cb7a6
CodeSandbox <support@codesandbox.io>
2023-10-24 09:55:40 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const usersRouter = require("./routes/users");
+const bodyparser = require("body-parser");
 const app = express();
 const port = 3000;
 
+// Ruta basica del servidor
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/users", usersRouter);
+// Body parser
+app.use(bodyparser.json());
 
+// Ruta para recibir datos del webhook
+app.post("/webhook", (req, res) => {
+  const data = req.body;
+
+  console.log(
+    "Datos del webhook recibidos en formato json:",
+    JSON.stringify(data, null, 2),
+  );
+
+  res.json({ mensaje: "Datos del webhook recibidos con éxito" });
+});
+// Inicia el servidor en el puerto especificado
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`API de webhook escuchando en el puerto ${port}`);
 });
