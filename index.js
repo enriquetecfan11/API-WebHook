5c850794145175aa76174d9bb927cf3b75e6330d
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-10-25 13:35:48 +0200

    data and packages

--- index.js
+++ index.js
@@ -1,41 +1,63 @@
 const express = require("express");
-const usersRouter = require("./routes/users");
 const bodyparser = require("body-parser");
 const app = express();
+const os = require('os');
 const si = require('systeminformation');
+const morgan = require('morgan');
 
+// Server Options
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined'));
 
 // Ruta basica del servidor
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+// Rutas del servidor
+const usersRouter = require("./routes/users");
+const webHoouksRouter = require("./routes/webHooksRoutes");
+
+// Rutas del servidor
+app.use("/users", usersRouter);
+app.use("/webhook", webHoouksRouter);
+
 // Body parser
 app.use(bodyparser.json());
 
-// Ruta para recibir datos del webhook
-app.post("/webhook", (req, res) => {
-  const data = req.body;
-
-  console.log(
-    "Datos del webhook recibidos en formato json:",
-    JSON.stringify(data, null, 2),
-  );
-
-  res.json({ mensaje: "Datos del webhook recibidos con éxito" });
-});
-
 // Funcion para obtener datos del sistema
 async function cpuData() {
-  try {
-    const data = await si.cpu();
-    // si.networkGatewayDefault().then(data => console.log(data));
-    const networkData = await si.networkInterfaces();
-    console.log('💻 CPU Information:' + data.brand + ' Cores ' + data.cores + ' Pysical Cores ' + data.physicalCores  + "\n");
-    console.log('🛜 ' + networkData[1].iface + ' IP: ' + networkData[1].ip4  + "\n");
-  } catch (e) {
-    console.log(e)
+  const date = new Date();
+  const hour = new Intl.DateTimeFormat('es', { hour: 'numeric', hour12: false }).format(date);
+  const minute = new Intl.DateTimeFormat('es', { minute: 'numeric' }).format(date);
+  const second = new Intl.DateTimeFormat('es', { second: 'numeric' }).format(date);
+
+  const cpu = await si.cpu();
+  const disk = await si.fsSize();
+  const ram = await si.mem();
+  const ip = Object.values(os.networkInterfaces())
+    .flatMap(iface => iface.filter(address => !address.internal && address.family === 'IPv4'))
+    .map(address => address.address);
+
+  const status = {
+    cpu,
+    disk,
+    ram,
+    ip
   }
+
+  console.log(`🟢  System Information: ${hour}:${minute}:${second}`);
+  console.log(
+    `🟢  IP status: ${status.ip} \n` +
+    `🟢  Server time: ${hour}:${minute}:${second} \n` +
+    `🟢  Server OS: ${os.platform()} \n` +
+    `🟢  Server total CPU: ${os.cpus()[0].model} \n` +
+    `🟢  Server total RAM: ${os.totalmem() / 1024 / 1024 / 1024} GB \n` +
+    `🟢  Server total disk: ${status.disk[0].size} GB \n` +
+    `🟢  Server RAM usage: ${status.ram.used / 1024 / 1024 / 1024} GB % \n` +
+    `🟢  Server disk usage: ${status.disk[0].used} GB \n`
+  );
 }
 
 
@@ -43,7 +65,16 @@ async function cpuData() {
 const port = 3000;
 
 // Inicia el servidor en el puerto especificado
-app.listen(port, () => {
-  cpuData();
-  console.log(`🚀 API de webhook escuchando en el puerto ${port}` + "\n");
-});
+async function startServer() {
+  app.listen(port, () => {
+    console.log(`🚀 API de webhook escuchando en el puerto ${port}` + "\n");
+  });
+}
+
+
+async function main() {
+  await cpuData();
+  await startServer();
+}
+
+main();
---
+++ routes/webHooksRoutes.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+
+// Crea un archivo JSON vacío si no existe
+const dataFilePath = "./data/webhook-data.json";
+
+if (!fs.existsSync(dataFilePath)) {
+  fs.writeFileSync(dataFilePath, "[]");
+}
+
+// Ruta para recibir datos del webhook
+router.post("/", (req, res) => {
+  const data = req.body;
+
+  // Genera un ID único para este conjunto de datos
+  const uniqueId = uuidv4();
+  data.id = uniqueId;
+
+  // Leer el archivo JSON existente
+  fs.readFile(dataFilePath, (err, existingData) => {
+    if (err) {
+      console.error("Error al leer el archivo JSON:", err);
+      return res.status(500).json({ error: "No se pudieron guardar los datos" });
+    }
+
+    // Parsear el contenido existente del archivo JSON (si existe)
+    const parsedData = existingData.length > 0 ? JSON.parse(existingData) : [];
+
+    // Agregar los nuevos datos al arreglo existente
+    parsedData.push(data);
+
+    // Escribir el arreglo actualizado en el archivo JSON
+    fs.writeFile(dataFilePath, JSON.stringify(parsedData, null, 2), (err) => {
+      if (err) {
+        console.error("Error al escribir en el archivo JSON:", err);
+        return res.status(500).json({ error: "No se pudieron guardar los datos" });
+      }
+
+      res.json({ mensaje: "Datos del webhook recibidos y guardados con éxito" });
+    });
+  });
+});
+
+// Ruta para obtener los datos almacenados
+router.get("/", (req, res) => {
+  fs.readFile(dataFilePath, (err, data) => {
+    if (err) {
+      console.error("Error al leer el archivo JSON:", err);
+      return res.status(500).json({ error: "No se pudieron obtener los datos" });
+    }
+
+    const parsedData = JSON.parse(data);
+    res.json(parsedData);
+  });
+});
+
+module.exports = router;
